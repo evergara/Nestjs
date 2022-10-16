5881b5d12c4f1ed01242417bdebee4f94b82e50b
Emerson Vergara <ee-web@hotmail.com>
2022-10-16 06:57:19 -0500

    Dev: @Param  manejo de parametros en la API's, para enviar parametros obligatorios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Param } from '@nestjs/common/decorators';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,37 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new')
+  newEndPoint() {
+    return 'It is new';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'with /sas/';
+  }
+
+  @Get('/product/:productID')
+  getProduct(@Param() Params: any) {
+    return `Product params ${Params.productID}`;
+  }
+
+  @Get('/products/:productID')
+  getProductID(@Param('productID') productID: string) {
+    return `Product direct ${productID}`;
+  }
+
+  @Get('/category/:categoryID/products/:productID')
+  getCategoryParam(@Param() params: any) {
+    return `Categories ${params.categoryID} por product ${params.productID} params`;
+  }
+  @Get('/categories/:categoryID/products/:productID')
+  getCategory(
+    @Param('categoryID') categoryID: string,
+    @Param('productID') productID: string,
+  ) {
+    return `Category; ${categoryID} por product ${productID} Direct`;
   }
 }
